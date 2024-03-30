cef7eb624317b552d3cabef49edf21b8b4d25cf0
YuliyaArkhipova <y.arhipova@bk.ru>
2024-03-31 01:10:23 +0300

    add homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,35 @@
 -- SQL-команды для создания таблиц
+
+CREATE DATABASE north
+
+CREATE TABLE employees #работники
+(
+    employee_id int PRIMARY KEY,
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL,
+    title varchar(100) NOT NULL,
+    birth_date date NOT NULL,
+    notes text
+);
+
+SELECT * FROM employees
+
+CREATE TABLE customers
+(
+    customer_id varchar(100) PRIMARY KEY,
+    company_name varchar(100) NOT NULL,
+    contact_name varchar(100) NOT NULL
+);
+
+SELECT * FROM customers
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+    customer_id varchar(100) REFERENCES customers(customer_id) NOT NULL,
+    employee_id int REFERENCES employees(employee_id) NOT NULL,
+    order_date date NOT NULL,
+    ship_city varchar(100) NOT NULL
+);
+
+SELECT * FROM orders
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,33 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+with psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="123456"
+) as connection:
+
+    with connection.cursor() as cursor:
+        with open (f'north_data/employees_data.csv') as csv_file:
+            header = next(csv_file)
+            reader = csv.reader(csv_file)
+            for row in reader:
+                cursor.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row)
+        with open (f'north_data/customers_data.csv') as csv_file:
+            header = next(csv_file)
+            reader = csv.reader(csv_file)
+            for row in reader:
+                cursor.execute("INSERT INTO customers VALUES (%s, %s, %s)", row)
+        with open (f'north_data/orders_data.csv') as csv_file:
+            header = next(csv_file)
+            reader = csv.reader(csv_file)
+            for row in reader:
+                cursor.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", row)
+
+connection.close()
+
+
+
